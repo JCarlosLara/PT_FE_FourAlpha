6e64ea631b7b5dff2ce4cbbe5d8a199fe94058ce
Juan Carlos Lara <jcarloslaraanguiano@gmail.com>
2023-06-12 21:57:49 -0600

    Primer draft de Frontend Blazor consumiendo API local

---
+++ Data/Response.cs
@@ -0,0 +1,15 @@
+﻿namespace FECoffeeShop.Data
+{
+    public class Response<T>
+    {
+        public int Success { get; set; }
+        public string Message { get; set; }
+        //public List<Category> Data { get; set; }
+        public T Data { get; set; }
+
+        public Response()
+        {
+            this.Success = 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using FECoffeeShop.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+//Intentando agregar inyección de dependencias para un HttpClient como antes se hacía en startup.cs, validar cómo cambiar esta URI para cuando ya esté arriba el API
+builder.Services.AddSingleton(new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:44369")
+});
+
+//truco para ignorar el certificado por el https NOTA: Validar si debo removerlo una vez en producción
+var myHttpClientHandler = new HttpClientHandler();
+myHttpClientHandler.ServerCertificateCustomValidationCallback =
+    (message, cert, chain, errors) => true;
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
